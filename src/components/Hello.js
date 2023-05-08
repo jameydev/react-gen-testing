04b69cec26f101d60ea1b39506425cd5ec832bd8
jameydev <jybryce4@gmail.com>
2023-05-08 09:14:15 -0500

    exported Hello component; demo'd es6 func comp

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,3 +1,9 @@
-function Hello() {
-    return <h1>Hello, World!</h1>
+// function Hello() {
+//     return <h1>Hello, World!</h1>;
+// }
+
+const Hello = () => {
+    return <h1>Hello there!</h1>;
 }
+
+export default Hello;
