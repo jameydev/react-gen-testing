6772597326ff7ede5dbac9461d1412a10118ce7f
jameydev <jybryce4@gmail.com>
2023-05-09 09:25:24 -0500

    working on props from tutorial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
-import Hello from './components/Hello';
-import Message from './components/Message';
+// import Hello from './components/Hello';
+// import Message from './components/Message';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
-      <Hello />
-      <Message />
+      {/* <Hello />
+      <Message /> */}
+      <Profile name='Jamey'/>
     </div>
   );
 }
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,8 +2,10 @@
 //     return <h1>Hello, World!</h1>;
 // }
 
+const name = 'Jamey';
+
 const Hello = () => {
-    return <h1>Hello there!</h1>;
+    return <h1>Hello there, {name}!</h1>;
 }
 
 export default Hello;
