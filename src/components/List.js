006f6ec7d053985d71468792469b9266f636dda5
jameydev <jybryce4@gmail.com>
2023-05-16 07:09:22 -0500

    array example shown

--- src/components/List.js
+++ src/components/List.js
@@ -7,16 +7,28 @@ export default function List(props) {
         return (<li key={item.id} className="list-item">{`${item.name} - $${item.price}`}</li>);
     });
 
+    // Example
+    const fruits = ["Apple", "Orange", "Banana"];
+    const fruitList = fruits.map((fruit, index) => {
+        return (<li key={index} className="list-item">{fruit}</li>);
+    });
+
     return ordered ? (
         <div className="list">
             <ol>
                 {itemList}
-            </ol>  
+            </ol>
+            <ol>
+                {fruitList}
+            </ol>
         </div>
     ) : (
         <div className="list">
             <ul>
                 {itemList}
+            </ul>
+            <ul>
+                {fruitList}
             </ul>
         </div>
     );
